d4604efd4805679082593d28a5e4009155beba9e
Brian Walters <brian.walters@nearform.com>
2025-03-05 12:40:31 -0600

    Create Todo entity

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Repository } from 'typeorm';
+import { Todo } from './entity/todo.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @InjectRepository(Todo)
+    private todoRepository: Repository<Todo>,
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return JSON.stringify(this.todoRepository.find(), null, 2);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './entity/todo.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      url: process.env.DATABASE_URL,
+      entities: [Todo],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
