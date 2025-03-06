dfc71899b6a8c3123d9e0f8e60b9aa6375cfac45
Brian Walters <brian.walters@nearform.com>
2025-03-06 17:08:52 -0600

    got partials and tailwind working

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setViewEngine('hbs');
+  hbs.registerPartials(join(__dirname, '..', 'views/partials'));
   await app.listen(3000);
 }
 bootstrap();
