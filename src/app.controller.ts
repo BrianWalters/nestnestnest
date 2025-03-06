f82a27ab9e4a12959678ea2faaa389d41877678c
Brian Walters <brian.walters@nearform.com>
2025-03-06 10:32:35 -0600

    get handlebars working

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  root(): string {
-    return JSON.stringify(this.appService.getTodos(), null, 2);
+  @Render('index')
+  root() {
+    return {
+      todos: this.appService.getTodos(),
+    };
   }
 }
